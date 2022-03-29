df4e2f7cff5d685d015e19b65a13d43d1bdb42b3
Minh Nguyen <hongminh1620@gmail.com>
2022-03-28 21:25:35 -0700

    Load big data

--- app/restfulApi/apps.py
+++ app/restfulApi/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
-
+from .load_heavy_data import load_model
 
 class RestfulapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'restfulApi'
+    def ready(self):
+        load_model()
