8b4ffe72e7f459fac315667f519291079bfe6101
Minh Nguyen <hongminh1620@gmail.com>
2022-03-28 21:26:18 -0700

    Add new endpoint

--- app/restfulApi/urls.py
+++ app/restfulApi/urls.py
@@ -1,13 +1,14 @@
 from django.urls import include, path
 from rest_framework import routers
-from . import views
+from .views import CompanyViewSet, SearchViewSet
 
 router = routers.DefaultRouter()
-router.register(r'companies', views.CompanyViewSet)
+router.register(r'companies', CompanyViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('search/', SearchViewSet.as_view(), name='search')
 ]
--- app/restfulApi/views.py
+++ app/restfulApi/views.py
@@ -1,9 +1,18 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .serializers import CompanySerializer
 from .models import company
+from .company_stats import get_all_charts
 
 # Create your views here.
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = company.objects.all().order_by('name')
     serializer_class = CompanySerializer
+
+class SearchViewSet(APIView):
+    def get(self, request):
+        company_name = dict(request.query_params)['company_name'][0]
+        approval_rate_over_years, years = get_all_charts(company_name)
+        return Response({"hi": approval_rate_over_years}, status=status.HTTP_200_OK)
