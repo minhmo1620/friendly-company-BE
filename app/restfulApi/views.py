475a89dc8c9d5e662b83ae1833013a89d90e70d2
Minh Nguyen <hongminh1620@gmail.com>
2022-04-19 12:00:48 -0700

    Create get_companies_list function to do the filter

--- app/restfulApi/views.py
+++ app/restfulApi/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from .serializers import CompanySerializer
 from .models import company
 from .company_stats import get_all_charts
+from .load_heavy_data import get_company_stats_compilation_data
 
 # Create your views here.
 class CompanyViewSet(viewsets.ModelViewSet):
@@ -25,16 +26,28 @@ class FilterViewSet(APIView):
         city = query_params['city'][0]
         job_type = query_params['job_type'][0]
 
-        companies = [
-            {
-                "name": "Google",
-                "approval_rate": "99",
-                "wage_range": [100, 200]
-            },
-            {
-                "name": "Facebook",
-                "approval_rate": "99",
-                "wage_range": [100, 200]
-            }
-        ]
+        companies = get_companies_list(job_industry, state, city, job_type)
         return Response({"companies": companies}, status=status.HTTP_200_OK)
+
+def get_companies_list(job_industry, state, city, job_type):
+    data = get_company_stats_compilation_data()
+
+    # Filter all companies with job_industry, state, city, job_type
+
+    # Return a list of companies with the format as below
+    companies = [
+        {
+            "name": "Google",
+            "approval_rate": "99",
+            "wage_range": [100, 200],
+            "average_wage": 150
+        },
+        {
+            "name": "Facebook",
+            "approval_rate": "99",
+            "wage_range": [100, 200],
+            "average_wage": 150
+        }
+    ]
+    return companies
+
