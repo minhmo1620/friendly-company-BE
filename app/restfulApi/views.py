caf8ee1cccb5da79688b45913c803e62a4750cfd
Kareem Alsayed <kareem@uni.minerva.edu>
2022-04-20 07:27:50 -0700

    creating filtering view

--- app/restfulApi/views.py
+++ app/restfulApi/views.py
@@ -2,8 +2,8 @@ from rest_framework import viewsets, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .serializers import CompanySerializer
-from .models import company
+from .serializers import CompanySerializer, CompanyDataSerializer
+from .models import company, company_data
 from .company_stats import get_all_charts
 from .load_heavy_data import get_company_stats_compilation_data
 
@@ -30,24 +30,22 @@ class FilterViewSet(APIView):
         return Response({"companies": companies}, status=status.HTTP_200_OK)
 
 def get_companies_list(job_industry, state, city, job_type):
-    data = get_company_stats_compilation_data()
-
+    job_type = job_type == 'Full-time'
     # Filter all companies with job_industry, state, city, job_type
+    companies_lst = company_data.objects.filter(
+        full_time = job_type,
+        state = state,
+        job_industry = job_industry
+    )
+    companies = []
+    for company in companies_lst:
+        entry = {}
+        entry['name'] = getattr(company, 'company_name')
+        entry['approval_rate'] = getattr(company, 'approval_rate')
+        entry['wage_range'] = [getattr(company, 'min_average'), getattr(company, 'max_average')]
+        entry['average_wage'] = getattr(company, 'average')
 
-    # Return a list of companies with the format as below
-    companies = [
-        {
-            "name": "Google",
-            "approval_rate": "99",
-            "wage_range": [100, 200],
-            "average_wage": 150
-        },
-        {
-            "name": "Facebook",
-            "approval_rate": "99",
-            "wage_range": [100, 200],
-            "average_wage": 150
-        }
-    ]
+        companies.append(entry)
+
     return companies
 
